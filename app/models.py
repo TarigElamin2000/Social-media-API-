34cdc7e888f24a3d2b3b212d9c05eb73132b54df
Tarig <tariq.baha558@gmail.com>
2023-11-06 10:07:30 -0800

    fixing buggs

--- app/models.py
+++ app/models.py
@@ -8,13 +8,13 @@ class Post(Base):
     __tablename__ = "posts"
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
-    content  = Column(String, nullable=False)
-    published = Column(Boolean, server_default='True', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), 
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default='TRUE', nullable=False)
+    created_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
-    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"),
-                       nullable=False)
-    
+    owner_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="CASCADE"), nullable=False)
+
     owner = relationship("User")
 
 class User(Base):
